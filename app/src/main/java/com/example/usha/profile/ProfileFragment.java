f33b7e96c0bf330dfd5cc55fbace9402fea24212
dosora1 <dosora9@gmail.com>
2022-07-18 05:47:03 +0900

    마이프로필 inflate 코드 추가
    자바로 작성

---
+++ app/src/main/java/com/example/usha/profile/ProfileFragment.java
@@ -0,0 +1,26 @@
+package com.example.usha.profile;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+
+import com.example.usha.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment(){
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
