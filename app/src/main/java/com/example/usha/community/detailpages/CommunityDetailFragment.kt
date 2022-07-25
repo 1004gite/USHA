62cbc0070baad071e851d367934bfa1ff772977c
1004gite <1004gite@naver.com>
2022-07-25 14:00:47 +0900

    community UI 구성

--- app/src/main/java/com/example/usha/community/detailpages/CommunityDetailFragment.kt
+++ app/src/main/java/com/example/usha/community/detailpages/CommunityDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.usha.community.detailpages
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +12,14 @@ import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.example.usha.R
+import com.example.usha.community.detailpages.viewpagers.DetailFragment
+import com.example.usha.community.detailpages.viewpagers.PagerStateFragmentAdapter
 import com.example.usha.community.model.CommunityItemModel
 import com.example.usha.databinding.FragmentCommunityDetailBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class CommunityDetailFragment() : Fragment() {
 
@@ -21,6 +27,7 @@ class CommunityDetailFragment() : Fragment() {
     private lateinit var binding: FragmentCommunityDetailBinding
     private lateinit var data: CommunityItemModel
     private lateinit var navController: NavController
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,7 +52,23 @@ class CommunityDetailFragment() : Fragment() {
     }
 
     fun setViewPager(){
-
+        viewPager = binding!!.communityPager
+        viewPager.adapter = PagerStateFragmentAdapter(this).apply {
+            addFragment(DetailFragment())
+            addFragment(DetailFragment())
+            addFragment(DetailFragment())
+            addFragment(DetailFragment())
+        }
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int){
+                super.onPageSelected(position)
+                Log.e("ViewPagerFragment", "Page ${position+1}")
+            }
+        })
+        // tablayout attach
+        TabLayoutMediator(binding!!.communiotyTabLayout, viewPager){ tab, position ->
+//            Log.e("tabLog", position.toString())
+        }.attach()
     }
 
 }
