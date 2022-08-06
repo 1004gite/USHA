22c6f8a348ed2baac7d3773c1deb8916bf86f7fd
1004gite <1004gite@naver.com>
2022-08-06 17:25:16 +0900

    커뮤니티 카드 이미지 둥글게

--- app/src/main/java/com/example/usha/community/recyclerview/CommunityViewHolder.kt
+++ app/src/main/java/com/example/usha/community/recyclerview/CommunityViewHolder.kt
@@ -13,8 +13,9 @@ import com.example.usha.databinding.CommunityItemBinding
 class CommunityViewHolder(private val itemBinding: CommunityItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
 
     fun bind(community: Community, position: Int) {
+//        itemBinding.root.clipToOutline = true
+//        itemBinding.imageView.clipToOutline = true // 배경에 맞게 사진 짜르기
         // 데이터바인딩을 위함
-        itemBinding.imageView.clipToOutline = true // 배경에 맞게 사진 짜르기
         itemBinding.community = community
     }
 
