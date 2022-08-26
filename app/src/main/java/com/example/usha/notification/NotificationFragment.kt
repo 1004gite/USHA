e78d141f014e232737b3272d72b22dd94431cc94
1004gite <1004gite@naver.com>
2022-08-26 17:12:46 +0900

    notification fragment 완성/ api없어서 틀만 완성함

--- app/src/main/java/com/example/usha/notification/NotificationFragment.kt
+++ app/src/main/java/com/example/usha/notification/NotificationFragment.kt
@@ -1,5 +1,6 @@
 package com.example.usha.notification
 
+import android.graphics.drawable.GradientDrawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,20 +8,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.usha.R
 import com.example.usha.databinding.FragmentNotificationBinding
 
 class NotificationFragment : Fragment() {
     private lateinit var viewModel: NotificationViewModel
     private lateinit var binding: FragmentNotificationBinding
+    private lateinit var adapter: NotificationRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_notification,container,false)
         viewModel = ViewModelProvider(this).get(NotificationViewModel::class.java)
-        binding.viewModel = viewModel
+        adapter = NotificationRecyclerAdapter(viewModel)
+        binding = DataBindingUtil.inflate<FragmentNotificationBinding?>(inflater,R.layout.fragment_notification,container,false).apply {
+            this.viewModel = this@NotificationFragment.viewModel
+            notificationRecyclerView.layoutManager = LinearLayoutManager(context)
+            notificationRecyclerView.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+            notificationRecyclerView.adapter = this@NotificationFragment.adapter
+        }
 
         return binding!!.root
     }
