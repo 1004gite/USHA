26b2e81417371caff240b08ffd7a7c6e8ba05174
dosora1 <dosora9@gmail.com>
2022-09-15 21:52:59 +0900

    링크복사 기능 추가
    -Toast 기능 추가
    -오픈채팅 링크 필요

--- app/src/main/java/com/example/usha/setting/SettingFragment.java
+++ app/src/main/java/com/example/usha/setting/SettingFragment.java
@@ -1,25 +1,26 @@
 package com.example.usha.setting;
+import static android.content.Context.CLIPBOARD_SERVICE;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.example.usha.R;
-import com.example.usha.databinding.FragmentProfileBinding;
 import com.example.usha.databinding.FragmentSettingBinding;
 
 public class SettingFragment extends Fragment {
     private FragmentSettingBinding binding;
-    private Button btn_terms,btn_policy;
-    static String WvUrl;
+    private TextView btn_terms,btn_policy,btn_copy;
+    static String WvUrl,CopyLink;
 
     @Override
     public View onCreateView (LayoutInflater inflater,
@@ -30,6 +31,9 @@ public class SettingFragment extends Fragment {
 
         btn_terms= binding.TermsBtn;
         btn_policy = binding.PolicyBtn;
+        btn_copy = binding.CopyBtn;
+        CopyLink = "https://www.headingwarm.com";
+
         //이용약관
         btn_terms.setOnClickListener(view1 -> {
             WvUrl = "https://www.headingwarm.com/term2";
@@ -39,6 +43,13 @@ public class SettingFragment extends Fragment {
         btn_policy.setOnClickListener(view2 -> {
             WvUrl = "https://www.headingwarm.com/term";
             Navigation.findNavController(view2).navigate(R.id.action_setting_to_terms);
+        });
+        //링크복사
+        btn_copy.setOnClickListener(view3 -> {
+            ClipboardManager clipboardManager = (ClipboardManager) getActivity().getSystemService(CLIPBOARD_SERVICE);
+            ClipData clipData = ClipData.newPlainText("copyText",CopyLink);
+            clipboardManager.setPrimaryClip(clipData);
+            Toast.makeText(getActivity().getApplicationContext(),"링크가 클립보드에 복사되었습니다.",Toast.LENGTH_SHORT).show();
         });
         return view;
     }
